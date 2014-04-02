c67e584f43576a8257307dcf1d0a06e724b8ee25
james2doyle <james2doyle@gmail.com>
2014-04-01 22:07:18 -0400

    add missing string prototype. add screenshot.

--- js/betterarea.js
+++ js/betterarea.js
@@ -1,6 +1,6 @@
 function makeEditor(element) {
   var myEditor = new Editor($(element).find('textarea')[0]),
-  tabSize = '  ';
+    tabSize = '  ';
 
   var insert = function(chars, s) {
     myEditor.insert(chars, function() {
@@ -9,20 +9,27 @@ function makeEditor(element) {
     return false;
   };
 
+  // => http://stackoverflow.com/a/7592235/1163000
+  String.prototype.capitalize = function(lower) {
+    return (lower ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function(a) {
+      return a.toUpperCase();
+    });
+  };
+
   myEditor.area.onkeydown = function(e) {
 
     var area = this,
-    sel = myEditor.selection();
+      sel = myEditor.selection();
 
     // Auto close for `(`
-      if (e.shiftKey && e.keyCode == 57) {
-        return insert('(' + sel.value + ')', sel);
-      }
+    if (e.shiftKey && e.keyCode == 57) {
+      return insert('(' + sel.value + ')', sel);
+    }
 
     // Auto close for `{`
-      if (e.shiftKey && e.keyCode == 219) {
-        return insert('{' + sel.value + '}', sel);
-      }
+    if (e.shiftKey && e.keyCode == 219) {
+      return insert('{' + sel.value + '}', sel);
+    }
 
     // Auto close for `[`
     if (e.keyCode == 219) {
@@ -64,7 +71,7 @@ function makeEditor(element) {
       // write a tag name without `<` or `>` then press your `Tab` key!
       if (sel.before.match(isTagName)) {
         var tagName = isTagName.exec(sel.before)[2],
-        before = sel.before.replace(isTagName, '$1');
+          before = sel.before.replace(isTagName, '$1');
 
         area.value = before + '<' + tagName + ' ></' + tagName + '>' + sel.after;
         myEditor.select(sel.start + 2, sel.start + 2);
@@ -94,21 +101,21 @@ function makeEditor(element) {
       if (sel.before.match(isListItem)) {
         var take = isListItem.exec(sel.before),
           list = /[0-9]+\./.test(take[3]) ? parseInt(take[3], 10) + 1 + '.' : take[3]; // <ol> or <ul> ?
-          myEditor.insert('\n' + take[2] + list + ' ');
-          return false;
-        } else {
-          var getIndentBefore = /(^|\n)( +)(.*?)$/.exec(sel.before),
+        myEditor.insert('\n' + take[2] + list + ' ');
+        return false;
+      } else {
+        var getIndentBefore = /(^|\n)( +)(.*?)$/.exec(sel.before),
           indentBefore = getIndentBefore ? getIndentBefore[2] : "";
-          if (sel.before.match(/[\[\{\(\<\>]$/) && sel.after.match(/^[\]\}\)\>\<]/)) {
-            myEditor.insert('\n' + indentBefore + tabSize + '\n' + indentBefore, function() {
-              myEditor.select(sel.start + indentBefore.length + tabSize.length + 1, sel.start + indentBefore.length + tabSize.length + 1);
-            });
-            return false;
-          }
-          myEditor.insert('\n' + indentBefore);
+        if (sel.before.match(/[\[\{\(\<\>]$/) && sel.after.match(/^[\]\}\)\>\<]/)) {
+          myEditor.insert('\n' + indentBefore + tabSize + '\n' + indentBefore, function() {
+            myEditor.select(sel.start + indentBefore.length + tabSize.length + 1, sel.start + indentBefore.length + tabSize.length + 1);
+          });
           return false;
         }
+        myEditor.insert('\n' + indentBefore);
+        return false;
       }
+    }
 
     // Right arrow was pressed
     if (e.keyCode == 39) {
@@ -141,7 +148,7 @@ function makeEditor(element) {
     },
     'ul-list': function() {
       var sel = myEditor.selection(),
-      added = "";
+        added = "";
       if (sel.value.length > 0) {
         myEditor.indent('', function() {
           myEditor.replace(/^[^\n\r]/gm, function(str) {
@@ -159,8 +166,8 @@ function makeEditor(element) {
     },
     'ol-list': function() {
       var sel = myEditor.selection(),
-      ol = 0,
-      added = "";
+        ol = 0,
+        added = "";
       if (sel.value.length > 0) {
         myEditor.indent('', function() {
           myEditor.replace(/^[^\n\r]/gm, function(str) {
@@ -179,9 +186,9 @@ function makeEditor(element) {
     },
     'link': function() {
       var sel = myEditor.selection(),
-      title = prompt('Link Title:', 'Link title goes here...'),
-      url = prompt('Link URL:', 'http://'),
-      placeholder = 'Your link text goes here...';
+        title = prompt('Link Title:', 'Link title goes here...'),
+        url = prompt('Link URL:', 'http://'),
+        placeholder = 'Your link text goes here...';
       if (url && url !== "" && url !== 'http://') {
         myEditor.wrap('[' + (sel.value.length === 0 ? placeholder : ''), '](' + url + (title !== "" ? ' \"' + title + '\"' : '') + ')', function() {
           myEditor.select(sel.start + 1, (sel.value.length === 0 ? sel.start + placeholder.length + 1 : sel.end + 1));
@@ -191,7 +198,7 @@ function makeEditor(element) {
     },
     'image': function() {
       var url = prompt('Image URL:', 'http://'),
-      alt = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')).replace(/[\-\_\+]+/g, " ").capitalize();
+        alt = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')).replace(/[\-\_\+]+/g, " ").capitalize();
       alt = alt.indexOf('/') < 0 ? decodeURIComponent(alt) : 'Image';
       if (url && url !== "" && url !== 'http://') {
         myEditor.insert('\n\n![' + alt + '](' + url + ')\n\n');
